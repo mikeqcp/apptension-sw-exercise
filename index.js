963aa58959e5bf39552d78b52666899940e40e2e
Michał Przyszczypkowski <michal@unit9.com>
2018-10-06 21:04:17 +0200

    sync counter with FB backend

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
+import firebase from 'firebase';
+const config = {
+  apiKey: "AIzaSyCBpLRPfasKZWbwefb2BajvQrtx7r-NWwY",
+  authDomain: "sw-excercise.firebaseapp.com",
+  databaseURL: "https://sw-excercise.firebaseio.com",
+  projectId: "sw-excercise",
+  storageBucket: "sw-excercise.appspot.com",
+  messagingSenderId: "508880139950"
+};
+firebase.initializeApp(config);
+const database = firebase.database();
+
 let counterValue = 0;
+
+firebase.database().ref('/counter').on('value', snapshot => {
+  counterValue = snapshot.val();
+  _updateDisplay();
+});
+
+const _updateDisplay = () => {
+  label.innerHTML = counterValue;
+};
+
+const _sendToDB = () => {
+  firebase.database().ref('/counter').set(counterValue);
+};
 
 window.onload = () => {
   const increaseBtn = document.querySelector('#increase_btn');
@@ -8,13 +33,11 @@ window.onload = () => {
   increaseBtn.addEventListener('click', () => {
     counterValue += 1;
     _updateDisplay();
+    _sendToDB();
   });
   decreaseBtn.addEventListener('click', () => {
     counterValue -= 1;
     _updateDisplay();
+    _sendToDB();
   });
-
-  const _updateDisplay = () => {
-    label.innerHTML = counterValue;
-  }
 };
